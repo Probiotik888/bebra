ec97983c1f299ac47193d1b8fed0e58eb1af497d
Emelyanov Sergey <229196-6>
2023-05-22 14:20:07 +0300

    com 6

--- bomba.c
+++ bomba.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+
+#include "askname.h"
 
 int main(int argc, char **argv)
 {
     char first[255], last[255];
-    printf("Enter your name: ");
-    fgets(name, 255, stdin);
-    printf("length = %d\n", strlen(name)); 
-    name[strlen(name)-1] = '\0';
-    printf("Now enter your last name: ");
-    gets(last);
-    printf("Hello %s %s!\n", first, last);
+
+    askname(first, last);
+
+    printf("Hello, %s %s!\n", first, last);
     return 0;
 }
