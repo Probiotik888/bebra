24daa1a1b2246cc937f5be254e4ca8c6c86cb1e7
Probiotik888 <79096947+Probiotik888@users.noreply.github.com>
2023-05-24 17:35:28 +0300

    Update bomba 2.c

--- bomba 2.c
+++ bomba 2.c
@@ -5,8 +5,8 @@ void askname(char *first, char *last)
 {
     cout << "Enter your first name: ";
     fgets(first, 255, stdin);
-    first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+    first[strlen(first)-1] = '\0';
 
     cout <<"Now enter your last name: ";
-    gets(last); /* buffer overflow? what's that? */
+    gets(last);
 }
